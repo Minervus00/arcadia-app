76d97cb8be1f8e1cdc159a429901109acd2120da
Minervus00 <fayouedraogo@protonmail.com>
2024-02-17 15:38:49 +0000

    update

--- app/src/main/java/com/example/arcadia/activities/MainActivity.java
+++ app/src/main/java/com/example/arcadia/activities/MainActivity.java
@@ -2,19 +2,22 @@ package com.example.arcadia.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Base64;
 import android.widget.Toast;
 
-import com.example.arcadia.R;
 import com.example.arcadia.databinding.ActivityMainBinding;
 import com.example.arcadia.utilities.Constants;
 import com.example.arcadia.utilities.PreferenceManager;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -28,6 +31,11 @@ public class MainActivity extends AppCompatActivity {
         preferenceManager = new PreferenceManager(getApplicationContext());
         loadUserDetails();
         getToken();
+        setListeners();
+    }
+
+    private void setListeners() {
+        binding.imageProfile.setOnClickListener(v -> signOut());
     }
 
     private void loadUserDetails() {
@@ -53,5 +61,22 @@ public class MainActivity extends AppCompatActivity {
         docRef.update(Constants.KEY_FCM_TOKEN, token)
                 .addOnSuccessListener(unused -> showToast("Token mis à jour avec succès"))
                 .addOnFailureListener(err -> showToast("Unable to update token"));
+    }
+
+    private void signOut() {
+        showToast("Déconnexion...");
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection(Constants.KEY_COLLECTION_USERS).document(
+                preferenceManager.getString(Constants.KEY_USER_ID)
+        );
+        HashMap<String, Object> updates = new HashMap<>();
+        updates.put(Constants.KEY_FCM_TOKEN, FieldValue.delete());
+        docRef.update(updates)
+                .addOnSuccessListener(unused -> {
+                    preferenceManager.clear();
+                    startActivity(new Intent(getApplicationContext(), SignInActivity.class));
+                    finish();
+                })
+                .addOnFailureListener(err -> showToast("Impossible de vous déconnecter"));
     }
 }
