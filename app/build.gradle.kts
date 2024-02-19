ff864d4d4f5665efaf4838af23d0b031abf57d7a
Minervus00 <fayouedraogo@protonmail.com>
2024-02-19 14:59:09 +0000

    funky

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -58,4 +59,10 @@ dependencies {
 
     // MultiDex
     implementation("androidx.multidex:multidex:2.0.1")
+
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.google.android.gms:play-services-auth:21.0.0")
+    implementation("com.github.TutorialsAndroid:GButton:v1.0.19")
+
+
 }
