b898b62441dbc27641314de428b9cd4b786b2786
Minervus00 <fayouedraogo@protonmail.com>
2024-03-24 18:00:29 +0000

    display users list

---
+++ app/src/main/java/com/example/arcadia/activities/UsersActivity.java
@@ -0,0 +1,16 @@
+package com.example.arcadia.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.arcadia.R;
+
+public class UsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+    }
+}
